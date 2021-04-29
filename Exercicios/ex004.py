e25a33695d843261f2f1f14a37a0f5960419a8d9
joao <joao.nilton@gmail.com>
2021-04-29 18:22:36 -0300

    ver_2_29042021

--- Exercicios/ex004.py
+++ Exercicios/ex004.py
@@ -1,5 +1,15 @@
-n = input('Digite algo:')
-print('É alfanumérico {}'.format(n.isalpha()))
+n = (input('Digite algo:'))
+print('O tipo primitivo desse valor é {}'.format ( type(n)))
+print('É alfabetico: {}'.format(n.isalpha()))
+print('Só tem espaços: {}'.format (n.isspace()))
+print('É um numero: {}'.format(n.isnumeric()))
+print('É alfanumerico: {}'.format(n.isalnum()))
+print('Está em maiúscula: {}'.format(n.isupper()))
+print('Está em minúscula: {}'.format(n.islower()))
+print('É printável: {}'.format(n.isprintable()))
+print('Está em capitalizada: {}'.format(n.istitle()))
+
 # Este comentário é do dia 16 de março de 2021
 # Este comentário é do dia 17 de março de 2021
 # Este comentário é do dia 26 de março de 2021
+# Este comentário é do dia 29 de Abril de 2021 - Retorno do Jedi
--- Exercicios/ex005.py
+++ Exercicios/ex005.py
@@ -1,5 +1,11 @@
-n = input('Digite algo:')
-print('É alfanumérico {}'.format(n.isalpha()))
-# Este comentário é do dia 16 de março de 2021
-# Este comentário é do dia 17 de março de 2021
-# Este comentário é do dia 26 de março de 2021
+n = (input('Digite algo:'))
+print('O tipo primitivo desse valor é {}'.format ( type(n)))
+print('É alfabetico: {}'.format(n.isalpha()))
+print('Só tem espaços: {}'.format (n.isspace()))
+print('É um numero: {}'.format(n.isnumeric()))
+print('É alfanumerico: {}'.format(n.isalnum()))
+print('Está em maiúscula: {}'.format(n.isupper()))
+print('Está em minúscula: {}'.format(n.islower()))
+print('É printável: {}'.format(n.isprintable()))
+print('Está em capitalizada: {}'.format(n.istitle()))
+
