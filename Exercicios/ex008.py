d570f27c165a0a7bba426d9d9745ae251ca96870
joao <joao.nilton@gmail.com>
2021-05-05 14:35:54 -0300

    ver_2_05052021

--- Exercicios/ex008.py
+++ Exercicios/ex008.py
@@ -1,8 +1,11 @@
 print('Este é um conversor de medidas de comprimento') 
-m = float('Digite o valor que quer em metros da medida que quer converter: ')
+m = float(input('Digite o valor que quer em metros da medida que quer converter: '))
 km = m * 0.001
 cm = m * 100
 mm = m *1000
 inch = m * 39.3701
 
-print(' {} metro(s) corresponde a {} Km'. format()) 
+print(' {} metro(s) corresponde a {} Km(s)'. format(m, km)) 
+print(' {} metro(s) corresponde a {} cm(s)'. format(m, cm)) 
+print(' {} metro(s) corresponde a {} mm(s)'. format(m, mm)) 
+print(' {} metro(s) corresponde a {} polegada(s)'. format(m, inch)) 
---
+++ Exercicios/ex009.py
@@ -0,0 +1,28 @@
+print('=' * 40)
+print('Este é um gerador de tabuada') 
+print('=' * 40)
+n = float(input('Digite qual o número que voce quer ver a multiplicação de 0 a 10: '))
+a = 0
+b = 1
+c = 2
+d = 3
+e = 4
+f = 5
+g = 6
+h = 7
+i = 8
+j = 9
+k = 10
+
+print(' {} * {: } = {}'. format(n , a , n * a)) 
+print(' {} * {: } = {}'. format(n , b , n * b))
+print(' {} * {: } = {}'. format(n , c , n * c)) 
+print(' {} * {: } = {}'. format(n , d , n * d))
+print(' {} * {: } = {}'. format(n , e , n * e))
+print(' {} * {: } = {}'. format(n , f , n * f))
+print(' {} * {: } = {}'. format(n , g , n * g))
+print(' {} * {: } = {}'. format(n , h , n * h))
+print(' {} * {: } = {}'. format(n , i , n * i))
+print(' {} * {: } = {}'. format(n , j , n * j))
+print(' {} * {} = {}'. format(n , k , n * k))
+print('=' * 40)
