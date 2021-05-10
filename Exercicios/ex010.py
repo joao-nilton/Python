9deb883dd5f39237abc21318b68597dd1cee4d27
joao <joao.nilton@gmail.com>
2021-05-10 16:26:24 -0300

    ver_2_10052021

---
+++ Exercicios/ex010.py
@@ -0,0 +1,13 @@
+print('=' * 40)
+print('Este é um conversor  de Reais em Dolar') 
+print('=' * 40)
+
+r = float(input('Digite o valor que você quer converter em Reais: '))
+d = float(input('Digite  o valor do dolar hoje: '))
+c = r / d
+print('{} Real(s) equivalem a {} Dolar(s)'.format(r,c))
+print('=' * 40)
+
+'''nome = input('Qual é o seu nome? ')
+print('Prazer em te conhecer {:=^20}'.format(nome))'''
+ 
---
+++ Exercicios/ex011.py
@@ -0,0 +1,14 @@
+print('=' * 40)
+print('Este é um conversor  de Reais em Dolar') 
+print('=' * 40)
+print("\n" * 1)
+
+h = float(input('Digite a altura da parede em metros: '))
+b = float(input('Digite a largura da parede em metros: '))
+r =  h * b
+a = r / 2
+
+print('A área a ser pintada corresponde a {} metro(s)quadrado(s).'.format(r), end=' ') 
+print('E serão necessários {} litro(s) de tinta'.format(a))
+print("\n" * 1)
+print('=' * 40)
