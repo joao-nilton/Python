3557a8965295546cb2a7cb34212446956e6918b0
joao <joao.nilton@gmail.com>
2021-05-15 15:17:36 -0300

    ver_2_15052021

--- Aula008/modulos.py
+++ Aula008/modulos.py
@@ -1,7 +1,53 @@
+
+import emoji
+print(emoji.emojize('Olá Mundo : sunglasses: ', use_aliases=True))
+
+
+# print('\U0001F601')
+
+'''
+import random
+num1 = random.randint(0, 60 )
+num2 = random.randint(0, 60 )
+num3 = random.randint(0, 60 )
+num4 = random.randint(0, 60 )
+num5 = random.randint(0, 60 )
+num6 = random.randint(0, 60 )
+
+print(num1, num2, num3, num4, num5, num6)
+'''
+
+'''
+
+import random
+num = random.random() # por padrão gera um número entre 0 e 1.
+num1 = random.randint(1, 10)
+print(num)
+print(num1)
+
+'''
+
+
+'''from math import sqrt, floor 
+
+num = float(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz quadrade de {} é {} '.format(num, raiz, ))
+print('A raiz quadrade de {} é {:.2f} '.format(num, raiz ))'''
+
+
+
+
+
+
+'''
 import math
 num = float(input('Digite um número: '))
 raiz = math.sqrt(num)
-#cos = math.cos(num)
-p = math.pow(num)
+# cos = math.cos(num)
+# p = math.pow(num)
 
-print('A raiz quadrade de {} é {}, o cosseno é {} e o quadrado é {}'.format(num, raiz, cos, p))
+print('A raiz quadrade de {} é {} '.format(num, raiz, ))
+print('A raiz quadrade de {} é {:.2f} '.format(num, raiz ))
+print('A raiz quadrade de {} é {} '.format(num, math.ceil(raiz) ))
+print('A raiz quadrade de {} é {} '.format(num, math.floor(raiz) ))'''
