a65edf84e11439106708906b5fa08275cae8dac2
joao <joao.nilton@gmail.com>
2021-05-15 13:46:24 -0300

    ver_1_15052021

---
+++ Aula008/modulos.py
@@ -0,0 +1,7 @@
+import math
+num = float(input('Digite um número: '))
+raiz = math.sqrt(num)
+#cos = math.cos(num)
+p = math.pow(num)
+
+print('A raiz quadrade de {} é {}, o cosseno é {} e o quadrado é {}'.format(num, raiz, cos, p))
