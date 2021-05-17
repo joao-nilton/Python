e0dff7783a5de83ee573e7c76444ce0abdb0306b
joao <joao.nilton@gmail.com>
2021-05-17 18:57:57 -0300

    ver_1_17052021

---
+++ Exercicios/ex016.py
@@ -0,0 +1,39 @@
+'''print('=' * 40)
+print('Este é um arredondador de números Reais') 
+print('=' * 40)
+
+r = float(input('Digite o número Real: '))
+
+print('A parte inteira do número é {:.0f}'.format(r,))
+print('=' * 40)'''
+
+'''import math
+
+print('=' * 40)
+print('Este é um arredondador de números Reais') 
+print('=' * 40)
+
+r = float(input('Digite o número Real: '))
+
+print('O valor digitado foi {} e a  parte inteira do número é {:.0f}'.format(r,math.trunc(r)))
+print('=' * 40)'''
+
+'''from math import trunc
+
+print('=' * 40)
+print('Este é um arredondador de números Reais') 
+print('=' * 40)
+
+r = float(input('Digite o número Real: '))
+
+print('O valor digitado foi {} e a  parte inteira do número é {:.0f}'.format(r, trunc(r)))
+print('=' * 40)'''
+
+print('=' * 40)
+print('Este é um arredondador de números Reais') 
+print('=' * 40)
+
+r = float(input('Digite o número Real: '))
+
+print('O valor digitado foi {} e a  parte inteira do número é {:.0f}'.format(r, int(r)))
+print('=' * 40)
