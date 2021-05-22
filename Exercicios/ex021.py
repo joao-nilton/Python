1bbc10a2409721c281606531b9514a14b0bf624c
joao <joao.nilton@gmail.com>
2021-05-22 19:50:13 -0300

    ver_2_22052021

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -5,14 +5,29 @@ pygame.mixer.music.load('oceansgull.mp3')
 pygame.music.play()
 pygame.event.wait()''' # não funcionou.
 
-import pygame
-print (pygame.ver)
-
 '''
+import pygame
+# print (pygame.ver)
 pygame.init()
 pygame.mixer.music.load('needles-n-pins.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
 
+
+
+
+from pygame import mixer, event
+mixer.init()
+mixer.music.load('oceansgull.mp3')
+mixer.music.play()
+event.wait()
+
 '''
 
+import pygame
+arquivo = 'needles-n-pins.mp3'
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(arquivo)
+pygame.mixer.music.play()
+pygame.event.wait()
