8602c37874ba50b5dabdde6de3089fdb50f692bf
joao <joao.nilton@gmail.com>
2021-06-08 18:28:12 -0300

    ver_1_08062021

--- Exercicios/ex028.py
+++ Exercicios/ex028.py
@@ -1,15 +1,19 @@
-import random
+#import random
+from random import randint
+from time import sleep
 print (' ')
-print('=--=' * 40)
+print('=--=' * 20)
 print('JOGO DA ADIVINHAÇÃO')
-print('=--=' * 40)
+print('=--=' * 20)
 print('Vou pensar num número entre 0 e 5. Tente adivinhar...')
-print('+--+' * 40)
+print('+--+' * 20)
 print (' ')
 
 num = int(input('Em que número eu pensei? '))
 print('PROCESSANDO...')
-num1 = random.randint(0, 5)
+sleep(3)
+#num1 = random.randint(0, 5)
+num1 = randint(0, 5)
 
 if num != num1:
     print('GANHEI! Eu pensei no número {} e não no {}'.format(num1, num))
