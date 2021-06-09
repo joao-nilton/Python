dfedd36517a7f69b897cb5d9328e40f767c44f8a
joao <joao.nilton@gmail.com>
2021-06-09 18:11:31 -0300

    ver_3_09062021

--- Exercicios/ex031.py
+++ Exercicios/ex031.py
@@ -3,7 +3,7 @@ print('=--=' * 20)
 print('PREÇO DA PASSAGEM RODOVIÁRIA')
 print('=--=' * 20)
 
-num = float(input('Qual a distância de sua viagem? '))
+'''num = float(input('Qual a distância de sua viagem? '))
 
 
 if num <= 200:
@@ -13,6 +13,13 @@ if num <= 200:
 else:
     print('Você está prestes a iniciar uma viagem de {} Km'.format(num))
     num2 = num * 0.45
-    print('E o preço da passagem é de {} Reais'.format(num2))
-    
+    print('E o preço da passagem é de {} Reais'.format(num2))'''
+
+print('OU...')
+
+distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você esta prestes a iniciar uma viagem de {}Km'.format(distancia))
+preco = distancia * .50 if distancia <= 200 else distancia * .45
+print('E o preço de sua passagem sera de R$ {:.2f}'.format(preco))
+
 print (' ')
