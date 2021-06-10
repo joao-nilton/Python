529403ec5ee017f9cc99a6855738e3d4bff92c3a
joao <joao.nilton@gmail.com>
2021-06-10 19:25:44 -0300

    ver_1_10062021

---
+++ Exercicios/ex033.py
@@ -0,0 +1,13 @@
+print (' ')
+print('=--=' * 20)
+print('Programa que lê três números e mostra qual é o maior e qual é o menor')
+print('=--=' * 20)
+
+primeiro = float(input('Digite o primeiro número: '))
+segundo = float(input('Digite o segundo número: '))
+terceiro = float(input('Digite o terceiro  número: '))
+print (' ')
+if primeiro > segundo :
+    print('O maior valor digitado foi {}'.format())
+else:
+    print('O ano {} NÃO É BISSEXTO')
