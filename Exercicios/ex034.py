25fc0a08c8f8506e6903678404ad3987ba17bcc0
joao <joao.nilton@gmail.com>
2021-06-12 18:19:34 -0300

    ver_2_12062021

---
+++ Exercicios/ex034.py
@@ -0,0 +1,16 @@
+print (' ')
+print('=--=' * 20)
+print('AUMENTO SALARIAL')
+print('=--=' * 20)
+from datetime import date
+
+sal = float(input('Digite o valor atual de seu salário'))
+print (' ')
+if sal > 1250:
+    sal1 = sal + (sal * 10)/100
+    print('O seu novo salário será de R$ {}'.format(sal1))
+
+else :
+    sal <= 1250
+    sal2 = sal + (sal * 15)/100
+    print('O seu novo salário será de R$ {}'.format(sal2))
