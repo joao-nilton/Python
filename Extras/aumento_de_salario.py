ce8de54e844b5e8bb0071874421c641689838636
joao <joao.nilton@gmail.com>
2021-06-14 20:22:33 -0300

    ver_1_14062021

--- Extras/aumento_de_salario.py
+++ Extras/aumento_de_salario.py
@@ -4,12 +4,13 @@ print('AUMENTO SALARIAL')
 print('=--=' * 20)
 
 sal = float(input('Digite o valor atual de seu salário: '))
+aum = float(input('Digite em porcentagem o valor do aumento: '))
 print (' ')
 if sal > 1250:
-    sal1 = sal + (sal * 10)/100
+    sal1 = sal + (sal * aum)/100
     print('O seu novo salário será de R$ {}'.format(sal1))
 
 else :
     sal <= 1250
-    sal2 = sal + (sal * 15)/100
+    sal2 = sal + (sal * aum)/100
     print('O seu novo salário será de R$ {}'.format(sal2))
