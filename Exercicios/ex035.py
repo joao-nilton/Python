a216dd42928ac5e20eec9c7e6961f2a1c4b43dec
joao <joao.nilton@gmail.com>
2021-06-16 19:39:01 -0300

    ver_2_16062021

--- Exercicios/ex035.py
+++ Exercicios/ex035.py
@@ -6,6 +6,8 @@ print('ANALISADOR DE TRIÂNGULOS')
 print('=--=' * 20)
 
 
+
+'''
 lado1 = float(input('Digite o primeiro número: '))
 lado2 = float(input('Digite o segundo número: '))
 lado3 = float(input('Digite o terceiro  número: '))
@@ -30,3 +32,23 @@ if lado1 > ((lado2 - lado3) or (lado3 - lado2)):
                     print (' ')
                     print ('Os valores {}, {} e {} não formam um triângulo'.format(lado1, lado2, lado3))
                     print (' ')
+                    '''
+
+
+
+print ('Ou...')
+
+lado1 = float(input('Digite o primeiro número: '))
+lado2 = float(input('Digite o segundo número: '))
+lado3 = float(input('Digite o terceiro  número: '))
+if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 +lado2:
+#if lado1 > lado2 - lado3 and lado2 > lado1 - lado3 and lado3 > lado1 - lado2: * não deu certo porque o valor da diferenca é em módulo.
+    print('Os segmentos acima podem formar um triângulo')
+else:
+    print('Os segmentos acima não podem formar um triângulo')
+
+
+ 
+#| b - c | < a < b + c
+#| a - c | < b < a + c
+#| a - b | < c < a + b
