c963a7ce9ce12caf549c0c70304096599a61fc96
joao <joao.nilton@gmail.com>
2021-06-21 18:30:39 -0300

    ver_1_21062021

---
+++ Extras/ex002c.py
@@ -0,0 +1,8 @@
+nome = input('Qual é o seu nome?')
+
+ 
+print('\033[32m Seja bem vindo(a)\033[m', nome)
+print('\033[31m É um prazer te conhecer \033[m \033[34m {} \033[m' .format (nome))
+
+
+# print('os valores são \033[32m{}\033[m e \033[31m{}\033[m!!!'.format(a,b))
