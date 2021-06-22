26a16c9e10766ad0217126655bf783448be8d556
joao <joao.nilton@gmail.com>
2021-06-22 18:44:18 -0300

    ver_2_22062021

--- Aula012/aula012.py
+++ Aula012/aula012.py
@@ -6,10 +6,12 @@ if nome == 'João':
     print('\033[32m Que nome bonito! \033[m')
 elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
     print('Seu nome é bem popular no Brasil! {}. '.format(nome))
-
+elif nome in 'Ana Cláudia Jéssica Juliana':
+    print('Belo nome feminino.')
 else:
     print('Seu nome é bem normal !')
 print('\033[34mTenha um bom dia {}! \033[m' .format(nome))
 print (' ')
 
+# o else é opcional para terminar uma estrutura condicional aninhada.
 
