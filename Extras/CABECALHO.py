d0d1ec84a731c1b53fd0de3a06ce24591f3b7c67
joao <joao.nilton@gmail.com>
2021-06-23 19:17:59 -0300

    ver_2_23062021

--- Extras/CABECALHO.py
+++ Extras/CABECALHO.py
@@ -6,26 +6,9 @@
 cores ={'limpa':'\033[m', 'branco':'\033[30m','vermelho':'\033[31m','verde':'\033[32m','amarelo':'\033[33m','azul':'\033[34m', 'mangenta':'\033[35m','ciano':'\033[36m','cinza':'\033[37m', 'pretoebranco':'\033[1;30m'}
 bkg ={'limpa':'\033[m', 'branco':'\033[40m','vermelho':'\033[41m','verde':'\033[42m','amarelo':'\033[43m','azul':'\033[44m', 'mangenta':'\033[45m','ciano':'\033[46m','cinza':'\033[47m', 'pretoebranco':'\033[1;40m'}
 
-"""###################################################################################################################"""
-
 print (' ')
 print('=' * 312)
 print('\033[1;31m TTITULO DO PROGRAMA \033[m')
 print('=' * 312)  
 print (' ')
-
-
-nome = str(input('\033[31mQual é o seu nome? \033[m'))
-if nome == 'João':
-    print('\033[32mQue nome bonito! \033[m')
-elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
-    print('Seu nome é bem popular no Brasil! {}. '.format(nome))
-elif nome in 'Ana Cláudia Jéssica Juliana':
-    print('Belo nome feminino.')
-else:
-    print('Seu nome é bem normal !')
-print('\033[34mTenha um bom dia {}! \033[m' .format(nome))
-print (' ')
-
-# o else é opcional para terminar uma estrutura condicional aninhada.
 
