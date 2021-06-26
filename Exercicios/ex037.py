5c237a45ede7a79d001814fb5f3649b68933b5a1
joao <joao.nilton@gmail.com>
2021-06-26 19:38:26 -0300

    ver_3_26052021

--- Exercicios/ex037.py
+++ Exercicios/ex037.py
@@ -15,18 +15,109 @@ print (' ')
 
 """#############################################################################################################"""
 
-num1 = input('Digite o primeiro numero: ')
-num2 = input('Digite o segundo numero: ')
-mysum = int(num1) + int(num2)
-num1a = int(num1, 2) + 0
-num1b = int(num2, 2) + 0 
-print(bin(mysum))
-print(bin(num1a))
-print(bin(num1b))
+print('Observe as opções abaixo.\n')
+print('1 : Converte decimal para binário')
+print('2 : Converte decimal para hexadecimal')
+print('3 : Converte decimal para octal\n')
 
-#num1 = "0b100"
-#num2 = "0b110"
-#num1 = input('Digite o primeiro numero: ')
-#num2 = input('Digite o segundo numero: ')
-#mysum = int(num1, 2) + int(num2, 2)
-#print(bin(mysum))
+sel = int(input('Selecione a opçao desejada: '))
+
+num1 = input('\nDigite o número que quer converter: ')
+
+if sel == 1:
+    num1a = int(num1, 10)
+    num1b= (bin(num1a))
+    print('\n{} em decimal equivale a {} em binário\n'.format(num1, num1b))
+elif sel == 2:
+    num2a = int(num1, 10)
+    num2b= (hex(num2a))
+    print('\n{} em decimal equivale a {} em hexadecimal\n'.format(num1, num2b))
+elif sel == 3:
+    num3a = int(num1, 10)
+    num3b= (oct(num3a))
+    print('\n{} em decimal equivale a {} em Octal\n'.format(num1, num3b))
+else:
+    print('\nOpçao inválida.\n')
+
+
+
+
+'''
+https://ichi.pro/pt/binario-hexadecimal-e-octal-em-python-35331013922529
+
+>>> num1 = "0b100"
+>>> num2 = "0b110"
+>>> mysum = int(num1, 2) + int(num2, 2)
+>>> print(bin(mysum))
+0b1010
+
+>>> num1 = 0b100
+>>> num2 = 0b110
+>>> mysum = num1 + num2
+>>> print(bin(mysum))
+>>> num1 = "0b100"
+>>> print(num1)
+"0b100"
+>>> num1 = 0b100
+>>> print(num1)
+4
+>>> print(bin(num1))
+0b100
+
+--------------------
+
+>>> hnum1 = "0x10"
+>>> hnum2 = "0x10"
+>>> myhsum = int(hnum1, 16) + int(hnum2, 16)
+>>> print(hnum1)
+"0x10"
+>>> print(myhsum)
+32
+>>> print(hex(myhsum))
+0x20
+>>> hnum1 = 0x10
+>>> hnum2 = 0x10
+>>> myhsum = hnum1 + hnum2
+>>> print(hnum1)
+16
+>>> print(myhsum))
+32
+>>> print(hex(myhsum))
+0x20
+
+----------------------------
+
+Os oito dígitos de octal são 0, 1, 2, 3, 4, 5, 6, 7.
+
+Vamos usar o mesmo exemplo de código aqui, mas usaremos a notação adequada e a função de conversão para octal.
+
+>>> onum1 = "0o10"
+>>> onum2 = "0o10"
+>>> myosum = int(onum1, 8) + int(onum2, 8)
+>>> print(onum1)
+"0o10"
+>>> print(myosum)
+16
+>>> print(oct(myosum))
+0o20
+>>> onum1 = 0o10
+>>> onum2 = 0o10
+>>> myosum = onum1 + onum2
+>>> print(onum1)
+8
+>>> print(myosum))
+16
+>>> print(oct(myosum))
+0o20
+
+--------------------------
+
+A grande vantagem do Python é que ele pode fazer quase tudo, exceto minha roupa. E estou trabalhando nisso.
+
+As lições são simples:
+
+O binário usa bin () e '0b'.
+Hexadecimal usa hex () e '0x'.
+Octal usa oct () e '0o'.
+A função int () pode ser usada para converter números em um inteiro de base 10 de qualquer base entre 2 e 36, alterando o segundo parâmetro. por exemplo, int (número, 30)
+'''
