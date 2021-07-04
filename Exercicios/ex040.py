b6ef6229aae8f452e93af5905c1677393ee556f8
joao <joao.nilton@gmail.com>
2021-07-04 15:00:57 -0300

    ver_2_04072021

---
+++ Exercicios/ex040.py
@@ -0,0 +1,30 @@
+# style --> 0 ,1 , 4, 7     0 = none    1 = bold    4 = underline       7 = negativo    3 = itálico
+# text --> 30 = branco      31 = vermelho   32 = verde  33 = amarelo    34 = azul   35 = vmagenta    36 = ciano 37 = cinza
+# back (background) mesma do text mas de 40 a 47
+# print('{} Muito prazer em te conhecer! {} SSS '.format(cores['pretoebranco'], cores['limpa']))
+#print('Olá! Muito prazer em te conhecer {} {} {}!!!,'.format(cores['pretoebranco'], nome, cores['limpa']))
+
+cores ={'limpa':'\033[m', 'branco':'\033[30m','vermelho':'\033[31m','verde':'\033[32m','amarelo':'\033[33m','azul':'\033[34m', 'mangenta':'\033[35m','ciano':'\033[36m','cinza':'\033[37m', 'pretoebranco':'\033[1;30m'}
+bkg ={'limpa':'\033[m', 'branco':'\033[40m','vermelho':'\033[41m','verde':'\033[42m','amarelo':'\033[43m','azul':'\033[44m', 'mangenta':'\033[45m','ciano':'\033[46m','cinza':'\033[47m', 'pretoebranco':'\033[1;40m'}
+
+print (' ')
+print('=' * 312)
+print ("  {:^150}  ".format('\033[1;31m"MEDIA ESCOLAR"\033[m'))
+print('=' * 312)  
+print (' ')
+
+"""#############################################################################################################"""
+
+nota1 = float(input('Digite a nota da primeira prova: '))
+nota2 = float(input('Digite a nota da segunda prova: '))
+nota3 = float(input('Digite a nota da terceira prova: '))
+nota4 = float(input('Digite a nota alcançada nos trabalhos: '))
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+if media < 5:
+    print('\nAluno REPROVADO')
+elif media >= 5 and media < 6.9: 
+    print('\nAluno em RECUPERAÇÃO')
+else:
+    print('\nAluno Aprovado')
