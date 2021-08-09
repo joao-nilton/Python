5a2538d7ab52ea5c3640944c87be95708c718a70
joao <joao.nilton@gmail.com>
2021-08-09 18:46:43 -0300

    ver_1_09082021 …

--- Extras/ex013c.py
+++ Extras/ex013c.py
@@ -14,17 +14,34 @@ print('\033[1;36m=\033[m' * 312)
 print (' ')
 
 """#############################################################################################################"""
-
-
-v = float(input('\033[1;35mDigite o valor da compra em Reais: \033[m'))
-d = float(input('\033[1;35mDigite o valor do desconto em porcentagem: \033[m'))
-p =  (v * d) / 100
-pg = v - p # ou novo preco = v - (v * d / 100) --> que substituiria as variáveis p e pg
-
-print('\n\033[1;33mO valor do desconto é de R$ {:.2f}.\033[m'.format(p), end=' ') 
-print('\033[1;33mVocê terá que pagar R$ {:.2f}\033[m'.format(pg))
 print("\n" * 1)
 print('=' * 40)
+print('Cálculo de reajuste salarial') 
+print('=' * 40)
+print("\n" * 1)
+
+v = float(input('Digite o valor do salário atual em Reais: '))
+d = float(input('Digite o valor do reajuste salarial em porcentagem: '))
+p =  (v * d) / 100
+pg = v + p  # ou novo preco = v - (v * d / 100) --> que substituiria as variáveis p e pg
+
+
+print('O valor corrigido do salário e de R$ {:.2f}'.format(pg))
+print("\n" * 1)
+print('=' * 40)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #print('\n\033[1;33mO valor do desconto é de R$ {:.2f}.\033[m'.format(p), end=' ') 
 #print('\033[1;33mVocê terá que pagar R$ {:.2f}\033[m'.format(pg))
